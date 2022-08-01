5d65d948e0754f0491108cfedd354c3cbb8cf135
Admin <Admin@DESKTOP-P6KQ8GT>
2022-08-01 20:54:24 +0530

    First commit

---
+++ ProductModule/src/main/java/c2tc_batch_product/ProductModuleApplication.java
@@ -0,0 +1,13 @@
+package c2tc_batch_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductModuleApplication.class, args);
+	}
+
+}
---
+++ ProductModule/src/test/java/c2tc_batch_product/ProductModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package c2tc_batch_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
